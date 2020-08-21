9e90dd0ab3138d1d6c6d2d2e02a8132df1c10a1b
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-08-21 13:29:37 +0530

    cast vote modals created

--- website/app/static/script/cast.js
+++ website/app/static/script/cast.js
@@ -1,3 +1,5 @@
+var name, party, candidate_id, imgSrc
+
 $('.btn-cast').click(function() {
     var i = $(this).attr('id').substring(14)
     showModal(i)
@@ -16,4 +18,23 @@ function showModal(i) {
     $('#modal-candidate-id').html(candidate_id)
     $('modal-img').attr('src', imgSrc)
     modal.modal()
+}
+
+function showConfirmModal() {
+    $('#modal').modal('hide')
+    var confirmModal = $('#confirm-modal')
+    $('#confirm-name').html(name)
+    $('#confirm-party').html(party)
+    $('#confirm-candidate-id').html(candidate_id)
+    $('#confirm-ing').attr('src', imgSrc)
+    confirmModal.modal({backdrop: 'static', keyboard: false})
+}
+
+function cancelVote() {
+    $('#confirm-modal').modal('hide')
+}
+
+function castVote() {
+    alert('casting your vote...')
+    
 }
