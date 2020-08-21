8a29aff23d6bdcdf6f510cb23cd3529b75599fb3
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-08-21 18:50:36 +0530

    minor updates

--- website/app/static/script/cast.js
+++ website/app/static/script/cast.js
@@ -1,40 +1,43 @@
 var name, party, candidate_id, imgSrc
 
-$('.btn-cast').click(function() {
-    var i = $(this).attr('id').substring(14)
-    showModal(i)
-})
 
 
-function showModal(i) {
+
+function showConfirmModal(i) {
     var candidate_div = $("#candidate-div-" + i)
     name = candidate_div.children('.name').html()
     party = candidate_div.children('.party').html()
     candidate_id = candidate_div.children('.candidate-id').html()
     imgSrc = candidate_div.children('.img').attr('src')
-    var modal = $("#modal")
-    $('#modal-name').html(name)
-    $('#modal-party').html(party)
-    $('#modal-candidate-id').html(candidate_id)
-    $('modal-img').attr('src', imgSrc)
-    modal.modal()
-}
-
-function showConfirmModal() {
-    $('#modal').modal('hide')
     var confirmModal = $('#confirm-modal')
     $('#confirm-name').html(name)
     $('#confirm-party').html(party)
     $('#confirm-candidate-id').html(candidate_id)
     $('#confirm-ing').attr('src', imgSrc)
-    confirmModal.modal({backdrop: 'static', keyboard: false})
+    confirmModal.modal()
 }
 
-function cancelVote() {
+function showKeyModal() {
     $('#confirm-modal').modal('hide')
+    var keyModal = $('#key-modal')
+    $('#key-name').html(name)
+    $('#key-party').html(party)
+    $('#key-candidate-id').html(candidate_id)
+    $('#key-ing').attr('src', imgSrc)
+    keyModal.modal({backdrop: 'static', keyboard: false})
+}
+
+function cancelConfirmModal() {
+    $("#confirm-modal").modal('hide')
+}
+
+function cancelKeyModal() {
+    $("#key-modal").modal('hide')
 }
 
 function castVote() {
-    alert('casting your vote...')
+    var voter_id = $('#voter_id').html()
+    var key = $('#key').val()
     
 }
+
